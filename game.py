57d620478419632187c6fa01da6bdefa0f9c1938
bobimbab <ruey.leong@gmail.com>
2023-10-26 19:25:49 +0800

    game logic and display images

--- game.py
+++ game.py
@@ -1,32 +1,65 @@
+import os
 
 
-Class Game:
+class GameLogic:
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.images = []
+        self.current_image_index = 0
+        self.questions = []
+        self.progress = 0
 
-    image_paths = ["image1.jpg", "image2.jpg", "image3.jpg"]
-    challenges = [
-        {"question": "What is the capital of France?", "answer": "Paris"},
-        {"question": "How many continents are there?", "answer": "7"},
-        {"question": "What is the largest planet in our solar system?", "answer": "Jupiter"}
-    ]
+    def load_images(self):
+        # Load the series of images based on the difficulty chosen
+        module_folder = os.path.join(os.path.dirname(__file__), "data")
+        image_folder = os.path.join(module_folder, "module_content")
+        image_txt_path = os.path.join(module_folder, "modules.txt")
 
-    current_image_index = 0
+        with open(image_txt_path, "r") as file:
+            image_lines = file.readlines()
+            images_by_difficulty = {}
 
+            for line in image_lines:
+                line = line.strip()
+                difficulty, images = line.split(":")
+                images_by_difficulty[difficulty] = [os.path.join(image_folder, image.strip()) for image in images.split(",")]
 
-    def get_current_image_path():
-        return image_paths[current_image_index]
+            self.images = images_by_difficulty.get(self.difficulty, [])
 
-    def get_current_challenge():
-        return challenges[current_image_index]
+    def load_questions(self):
+        # Load the questions after each series of images
+        with open("data/challenges.txt", "r") as file:
+            question_lines = file.readlines()
+            for line in question_lines:
+                question = line.strip()
+                self.questions.append(question)
 
-    def next_content():
-        global current_image_index
-        if current_image_index < len(image_paths) - 1:
-            current_image_index += 1
+    def save_progress(self):
+        # Save the progress to a text file
+        with open("data/progress.txt", "w") as file:
+            file.write(str(self.progress))
 
-    def prev_content():
-        global current_image_index
-        if current_image_index > 0:
-            current_image_index -= 1
+    def get_current_image(self):
+        # Get the current image based on the current_image_index
+        return self.images[self.current_image_index]
 
-    def is_answer_correct(answer):
-        return answer.lower() == challenges[current_image_index]["answer"].lower()
+    def get_current_question(self):
+        # Get the current question based on the current_image_index
+        return self.questions[self.current_image_index]
+
+    def next_image(self):
+        # Move to the next image
+        if self.current_image_index < len(self.images) - 1:
+            self.current_image_index += 1
+            self.progress += 1
+
+    def previous_image(self):
+        # Move to the previous image
+        if self.current_image_index > 0:
+            self.current_image_index -= 1
+            self.progress -= 1
+
+    def answer_question(self, answer):
+        # Handle the user's answer to the question
+        # You can implement your own logic here
+        pass
--- game_frame.py
+++ game_frame.py
@@ -1,67 +1,118 @@
 import tkinter as tk
-from tkinter import Label, Button, Entry
 from PIL import Image, ImageTk
-from game import Game
+from game import GameLogic
 
-class Game_frame:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Game")
-        self.root.geometry("400x400")
+class GameFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.game_logic = None
+        self.navigation_frame = None
+        self.current_image = None  # Store the current image as an attribute
 
-        self.label = Label(self.root)
-        self.label.pack()
+        self.difficulty_var = tk.StringVar()
+        self.difficulty_var.set("easy")  # Default difficulty
 
-        self.question_label = Label(self.root, text="", font=("Arial", 12))
-        self.question_label.pack()
+        self.create_widgets()
 
-        self.answer_entry = Entry(self.root)
-        self.answer_entry.pack()
+    def create_widgets(self):
+        # Create and place the navigation frame
+        self.navigation_frame = tk.Frame(self.parent)
+        self.navigation_frame.grid(row=0, column=0, columnspan=2)
 
-        self.next_button = Button(self.root, text="Next", command=self.next_image)
-        self.prev_button = Button(self.root, text="Previous", command=self.prev_image)
+        self.difficulty_label = tk.Label(self, text="Choose Difficulty:")
+        self.difficulty_label.grid(row=1, column=0)
 
-        self.next_button.pack()
-        self.prev_button.pack()
+        difficulty_options = ["easy", "medium", "hard"]
+        self.difficulty_menu = tk.OptionMenu(self, self.difficulty_var, *difficulty_options)
+        self.difficulty_menu.grid(row=1, column=1)
 
-        self.show_content()
-        self.show_challenge()
+        self.start_button = tk.Button(self, text="Start Game", command=self.start_game)
+        self.start_button.grid(row=2, column=0, columnspan=2)
 
-    def show_content(self):
-        image_path = Game.get_current_image_path()
-        img = Image.open(image_path)
-        img = img.resize((300, 300))
-        photo = ImageTk.PhotoImage(img)
+        self.canvas = tk.Canvas(self)
+        self.canvas.grid(row=3, column=0, columnspan=2)
 
-        self.label.config(image=photo)
-        self.label.image = photo
+        self.question_label = tk.Label(self, text="")
+        self.question_label.grid(row=4, column=0, columnspan=2)
 
-    def show_challenge(self):
-        challenge = Game.get_current_challenge()
-        self.question_label.config(text=challenge["question"])
-        self.answer_entry.delete(0, "end")  # Clear the answer entry field
+        self.previous_button = tk.Button(self, text="Previous", command=self.previous_image)
+        self.next_button = tk.Button(self, text="Next", command=self.next_image)
 
-    def next_content(self):
-        Game.next_content()
-        self.show_content()
-        self.show_challenge()
+        self.create_exit_button()  # Create the exit button
 
-    def prev_content(self):
-        Game.prev_content()
-        self.show_content()
-        self.show_challenge()
+    def create_exit_button(self):
+        self.exit_button = tk.Button(self, text="Exit", command=self.exit_game)
+        self.exit_button.grid(row=5, column=0, columnspan=2)
+        # Show the exit button by default
+        self.exit_button.grid()
 
-    def check_answer(self):
-        answer = self.answer_entry.get()
-        if Game.is_answer_correct(answer):
-            # Handle correct answer, e.g., show a message or move to the next image.
-            print("Correct answer!")
-            self.next_image()
-        else:
-            # Handle incorrect answer, e.g., show an error message.
-            print("Incorrect answer!")
+    def start_game(self):
+        difficulty = self.difficulty_var.get()
+        self.game_logic = GameLogic(difficulty)
+        self.game_logic.load_images()
+        self.game_logic.load_questions()
+        self.display_current_image()
+
+        # Clear the "Choose Difficulty" label and the "Start Game" button
+        self.difficulty_label.destroy()
+        self.difficulty_menu.destroy()
+        self.start_button.destroy()
+
+        self.previous_button.grid(row=5, column=0)  # Show the previous button
+        self.next_button.grid(row=5, column=1)  # Show the next button
+
+        self.create_exit_button()  # Create the exit button after the game starts
+
+    def display_current_image(self):
+        current_image_path = self.game_logic.get_current_image()
+        image = Image.open(current_image_path)
+        resized_image = self.resize_image(image)
+
+        self.canvas.delete("all")  # Clear canvas before drawing new image
+        self.current_image = resized_image  # Store the PhotoImage object
+        self.canvas.create_image(0, 0, anchor="nw", image=self.current_image)
+    def resize_image(self, image):
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+        image_width, image_height = image.size
+
+        # Calculate the aspect ratio to maintain the image's original proportions
+        aspect_ratio = min(canvas_width / image_width, canvas_height / image_height)
+        new_width = int(image_width * aspect_ratio)
+        new_height = int(image_height * aspect_ratio)
+
+        resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+        return ImageTk.PhotoImage(resized_image)
+
+    def display_current_question(self):
+        current_question = self.game_logic.get_current_question()
+        self.canvas.grid_forget()
+        self.question_label.config(text=current_question)
+
+        # Update the grid layout to properly position the buttons
+        self.previous_button.grid(row=6, column=0)
+        self.next_button.grid(row=6, column=1)
+        self.exit_button.grid_remove()  # Remove the duplicate exit button
+
+    def next_image(self):
+        self.game_logic.next_image()
+        self.display_current_image()
+        if self.game_logic.current_image_index == len(self.game_logic.images) - 1:
+            self.display_current_question()
+
+    def previous_image(self):
+        self.game_logic.previous_image()
+        self.display_current_image()
+
+    def exit_game(self):
+        self.parent.destroy()  # Destroy the current frame and exit the application
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = Game_frame(root)
+    root.geometry("1000x800")
+
+    game_frame = GameFrame(root)
+    game_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)  # Place the frame in the center
+
     root.mainloop()
