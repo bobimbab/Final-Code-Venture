c7658edb9848c866492f8d85ad0a39880390625f
Zhi Enn <zton0006@student.monash.edu>
2023-11-01 00:20:58 +0800

    progress tracker update

--- progress_tracker_frame.py
+++ progress_tracker_frame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from game import Game
 
 
 class ProgressTrackerFrame(tk.Frame):
@@ -7,17 +8,20 @@ class ProgressTrackerFrame(tk.Frame):
         self.master = master
         self.user_obj = user_obj
         self.young_learner = young_learner
+        self.game = Game(user_obj._username)
 
         self.progress_label = tk.Label(self, text="")
         self.progress_label.pack()
 
         progress_label = tk.Label(self, text="Progress: ")
-        progress_label.pack(side=tk.TOP)
+        progress_label.pack()
 
-        # Back to login page button
+        self.show_progress()
+
+        # Back to learner page
         # back_button = tk.Button(self, text="Back", command=lambda: login_frame.place())
         back_button = tk.Button(master=self, text="Back", command=self.return_to_younglearner)
-        back_button.pack(side=tk.BOTTOM)
+        back_button.pack()
 
     def return_to_younglearner(self):
         # self.show_progress()
@@ -27,13 +31,18 @@ class ProgressTrackerFrame(tk.Frame):
         self.young_learner.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
     def show_progress(self):
-        progress = self.user_obj.get_progress()  # Replace with your method to retrieve progress
+        progress = self.game.progress
         message = "Your progress:\n"
-        if self.user_obj.completed:
-            for module in progress:
-                message += f"{module}: Completed\n"
-        else:
-            for module, count in progress.items():
-                message += f"{module}: {count} images viewed\n"
+        for module, count in progress.items():
+            message += f"{module}: {count} images viewed\n"
         self.progress_label.config(text=message)
+        # progress = self.game.progress
+        # message = "Your progress:\n"
+        # if self.user_obj.completed:
+        #     for module in progress:
+        #         message += f"{module}: Completed\n"
+        # else:
+        #     for module, count in progress.items():
+        #         message += f"{module}: {count} images viewed\n"
+        # self.progress_label.config(text=message)
 
