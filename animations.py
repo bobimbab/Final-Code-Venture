4ee01f907433925c0d87c8e08f778a3258c7fac2
bobimbab <ruey.leong@gmail.com>
2023-11-01 23:27:43 +0800

    animation doc

--- animations.py
+++ animations.py
@@ -1,9 +1,18 @@
 import tkinter as tk
-import random
 
 
 class AnimatedButton(tk.Button):
+    """
+    AnimatedButton is a custom button widget with a pulsating animation.
+    """
     def __init__(self, master=None, **kwargs):
+        """
+        Initialize the AnimatedButton.
+
+        Args:
+            master (tkinter.Tk, optional): The parent widget. Defaults to None.
+            **kwargs: Additional keyword arguments for the Button widget.
+        """
         super().__init__(master, **kwargs)
         self.normal_font_size = 13
         self.max_font_size = 15
@@ -12,8 +21,10 @@ class AnimatedButton(tk.Button):
         self.animate()
 
     def animate(self):
-        current_font_size = self.cget("font").split()[1]
-        current_font_size = int(current_font_size)
+        """
+        Animate the button by changing its font size.
+        """
+        current_font_size = int(self.cget("font").split()[1])
 
         if self.increasing:
             current_font_size += 1
@@ -28,9 +39,20 @@ class AnimatedButton(tk.Button):
         self.config(font=new_font)
         self.after(500, self.animate)
 
-
 class FadingLabel(tk.Label):
+    """
+    FadingLabel is a custom label widget with a fading animation.
+    """
     def __init__(self, master=None, text="", font=("Arial", 14), duration=2000):
+        """
+        Initialize the FadingLabel.
+
+        Args:
+            master (tkinter.Tk, optional): The parent widget. Defaults to None.
+            text (str): The label text.
+            font (tuple, optional): The font settings. Defaults to ("Arial", 14).
+            duration (int, optional): The duration of the fade-out animation in milliseconds. Defaults to 2000.
+        """
         super().__init__(master, text=text, font=font)
         self.text = text
         self.duration = duration
@@ -39,12 +61,15 @@ class FadingLabel(tk.Label):
         self.fade_out()
 
     def fade_out(self):
+        """
+        Perform the fade-out animation.
+        """
         if self.alpha > 0:
             if self.text == "Correct":
-                #change color to green
+                # Change color to green
                 self.configure(fg=f'#00{self.alpha:02x}00')
             elif self.text == "Incorrect":
-                #change color to red
+                # Change color to red
                 self.configure(fg=f'#{self.alpha:02x}0000')
             self.alpha -= 1
             self.after(self.fade_out_interval, self.fade_out)
